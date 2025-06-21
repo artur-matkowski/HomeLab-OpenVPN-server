2355d3b4db5007afdca22e8c99e9f4fa6bfa7e6c
Artur Matkowski <artur.matkowski.zan@gmail.com>
2025-06-21 11:36:10 +0200

    fixing host_init.sh rule sets for routing

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # inside entrypoint.sh
-host_init.sh "${OPENVPN_HOST_NETWORK}"/24 \
-             "$(get_interface.sh "${OPENVPN_NETWORK}")" \
+host_init.sh "${OPENVPN_NETWORK}"/24 \
+             tun0 \
              "$(get_interface.sh "${OPENVPN_HOST_NETWORK}")"
 exec /init_vpn.sh "$@"
 
